e1b8c26055e235e14f5a25c7d95ea41b7c160a49
nep0x0 <nepaka15@gmail.com>
2021-09-11 08:45:25 +0700

    main

--- assets/styles/viewmore.js
+++ assets/styles/viewmore.js
@@ -4,7 +4,7 @@ const text = document.querySelector(".text");
 readMoreBtn.addEventListener("click", (e) => {
   text.classList.toggle("show-more");
   if (readMoreBtn.innerText === "Read More") {
-    readMoreBtn.innerText = "Read Les";
+    readMoreBtn.innerText = "Read Less";
   } else {
     readMoreBtn.innerText = "Read More";
   }

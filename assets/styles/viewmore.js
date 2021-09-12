00774541c5949d2a04aafd30ae63c65e5194f431
nep0x0 <nepaka15@gmail.com>
2021-09-12 19:20:43 +0700

    add new article; fix multiple read more;

--- assets/styles/viewmore.js
+++ assets/styles/viewmore.js
@@ -1,12 +1,22 @@
 
-const readMoreBtn = document.querySelector(".read-more-btn");
-const text = document.querySelector(".text");
 
-readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("show-more");
-  if (readMoreBtn.innerText === "Read More") {
-    readMoreBtn.innerText = "Read Less";
-  } else {
-    readMoreBtn.innerText = "Read More";
-  }
+let noOfCharac = 150;
+let contents = document.querySelectorAll(".content");
+contents.forEach(content => {
+    //If text length is less that noOfCharac... then hide the read more button
+    if(content.textContent.length < noOfCharac){
+        content.nextElementSibling.style.display = "none";
+    }
+    else{
+        let displayText = content.textContent.slice(0,noOfCharac);
+        let moreText = content.textContent.slice(noOfCharac);
+        content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+    }
 });
+
+function readMore(btn){
+    let post = btn.parentElement;
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
+}
